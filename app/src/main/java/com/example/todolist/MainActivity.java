fab499c717f172ec3802da55f4c9b521d182f7c7
Kritika0220 <kritishah2017@gmail.com>
2021-01-14 21:31:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.todolist.Adapter.ToDoAdapter;
+import com.example.todolist.Model.ToDoModel;
+import com.example.todolist.Utils.DatabaseHandler;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+    private FloatingActionButton fab;
+
+    private List<ToDoModel> tasklist;
+    private DatabaseHandler db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        db = new DatabaseHandler(this);
+        db.openDatabase();
+
+        tasklist = new ArrayList<>();
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerview);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(db,MainActivity.this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        fab = findViewById(R.id.fab);
+
+        ItemTouchHelper itemTouchHelper = new
+                ItemTouchHelper(new RecyclerItemTouchHelper(tasksAdapter));
+        itemTouchHelper.attachToRecyclerView(tasksRecyclerView);
+
+//        ToDoModel task = new ToDoModel();
+//        task.setTask("This is a Test Task");
+//        task.setStatus(0);
+//        task.setId(1);
+//
+//        tasklist.add(task);
+//        tasklist.add(task);
+//        tasklist.add(task);
+//        tasklist.add(task);
+//        tasklist.add(task);
+//        tasklist.add(task);
+//
+//        tasksAdapter.setTasks(tasklist);
+
+        tasklist = db.getAllTasks();
+        Collections.reverse(tasklist);
+        tasksAdapter.setTasks(tasklist);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+            }
+        });
+
+    }
+
+    public void handleDialogClose(DialogInterface dialog){
+        tasklist = db.getAllTasks();
+        Collections.reverse(tasklist);
+        tasksAdapter.setTasks(tasklist);
+        tasksAdapter.notifyDataSetChanged();
+    }
+}
